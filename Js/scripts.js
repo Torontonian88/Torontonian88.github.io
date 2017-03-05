f3a37be6a3a858b5bf02e537b688548652403b46
Faiza Razi <faiza.razi88@gmail.com>
2017-03-05 15:22:31 -0500

    web

---
+++ Js/scripts.js
@@ -0,0 +1,3 @@
+function myFunction() { //This is the function and the alert message//
+alert("Thank you for visiting! Your feedback means everything. Please leave a comment or reach out on LinkedIn. Have a wonderful day!");
+}
